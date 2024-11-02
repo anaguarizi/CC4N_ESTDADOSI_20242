67f986f043fa1275f087816c35870fb2e2777fce
Ana Clara <anaguarizidev@gmail.com>
2024-11-02 00:14:32 -0300

    Update selectionsort.c

--- Atividade 1/Selection Sort/selectionsort.c
+++ Atividade 1/Selection Sort/selectionsort.c
@@ -9,42 +9,43 @@
 // função selection sort
 void selectionSort(int *arr, int size) {
     for (int i = 0; i < size - 1; i++) {
-        int *min_ptr = arr + i;
+        int min_idx = i;
         for (int j = i + 1; j < size; j++) {
-            if (*(arr + j) < *min_ptr) {
-                min_ptr = arr + j;
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
             }
         }
-        int temp = *min_ptr;
-        *min_ptr = *(arr + i);
-        *(arr + i) = temp;
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
     }
 }
 
 // função pra printar o vetor
-void printArray(int *arr, int size) {
+void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++) {
-        printf("%d ", *(arr + i));
+        printf("%d ", arr[i]);
     }
 }
 
 // main pra rodar o programa
 int main() {
     int arr[SIZE];
+    int *arrPtr = arr;
     srand(time(NULL));
 
     // preenchendo o vetor com 50 valores
     for (int i = 0; i < SIZE; i++) {
-        *(arr + i) = rand() % 100; // valores aleatórios de 0 a 99
+        arrPtr[i] = rand() % 100; // valores aleatórios de 0 a 99
     }
 
     printf("Vetor original:\n");
-    printArray(arr, SIZE);
+    printArray(arrPtr, SIZE);
 
-    selectionSort(arr, SIZE);
+    selectionSort(arrPtr, SIZE);
 
     printf("\n\nVetor ordenado com Selection Sort:\n");
-    printArray(arr, SIZE);
+    printArray(arrPtr, SIZE);
 
     return 0;
 }
