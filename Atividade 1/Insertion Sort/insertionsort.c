543e9ea1025b44cc97220e7ee55253f355eb964a
Ana Clara <anaguarizidev@gmail.com>
2024-11-02 00:16:38 -0300

    Update insertionsort.c

--- Atividade 1/Insertion Sort/insertionsort.c
+++ Atividade 1/Insertion Sort/insertionsort.c
@@ -9,40 +9,41 @@
 // função insertion sort
 void insertionSort(int *arr, int size) {
     for (int i = 1; i < size; i++) {
-        int key = *(arr + i);
+        int key = arr[i];
         int j = i - 1;
-        while (j >= 0 && *(arr + j) > key) {
-            *(arr + j + 1) = *(arr + j);
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
             j--;
         }
-        *(arr + j + 1) = key;
+        arr[j + 1] = key;
     }
 }
 
 // função pra printar o vetor
 void printArray(int *arr, int size) {
     for (int i = 0; i < size; i++) {
-        printf("%d ", *(arr + i));
+        printf("%d ", arr[i]);
     }
 }
 
 // main pra rodar o programa
 int main() {
     int arr[SIZE];
+    int *arrPtr = arr;
     srand(time(NULL));
 
     // preenchendo o vetor com 50 valores
     for (int i = 0; i < SIZE; i++) {
-        *(arr + i) = rand() % 100; // valores aleatórios de 0 a 99
+        arrPtr[i] = rand() % 100; // valores aleatórios de 0 a 99
     }
 
     printf("Vetor original:\n");
-    printArray(arr, SIZE);
+    printArray(arrPtr, SIZE);
 
-    insertionSort(arr, SIZE);
+    insertionSort(arrPtr, SIZE);
 
     printf("\n\nVetor ordenado com Insertion Sort:\n");
-    printArray(arr, SIZE);
+    printArray(arrPtr, SIZE);
 
     return 0;
 }
